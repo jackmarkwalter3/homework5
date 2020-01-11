e808e9823af33c8ae4bd5855b3a385046356d682
Jack Markwalter <jackmarkwalter3@gmail.com>
2020-01-10 22:14:09 -0500

    display today's date

---
+++ script.js
@@ -0,0 +1,6 @@
+var weekDay = moment([]).format("dddd");
+var month = moment([]).format("MMMM");
+var dayOfMonth = moment([]).format("Do");
+
+$("#currentDay").text(weekDay + ", " + month + " " + dayOfMonth)
+
